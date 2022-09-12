e191ca62c33ad766145fa445289c6d647cc4b66f
yuchia329 <yuchia.chang@outlook.com>
2022-09-13 01:44:37 +0800

    init flutter_app branch

--- app.js
+++ app.js
@@ -1,167 +1,29 @@
-const {MongoClient} = require('mongodb');
-const assert = require('assert');
-const { getCurrentTime } = require('./datetimeHelper');
+const httpServer = require("http").createServer();
+const socketIO = require("socket.io")(httpServer);
 
-// Connection URI
-const uri = "mongodb://127.0.0.1:27017";
+socketIO.on("connection", function (client) {
+  console.log("Connected...", client.id);
 
-// Create a new MongoClient
-const client = new MongoClient(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+  //listens for new messages coming in
+  client.on("message", function name(data) {
+    console.log(data);
+    socketIO.emit("message", data);
+  });
+
+  //listens when a user is disconnected from the server
+  client.on("disconnect", function () {
+    console.log("Disconnected...", client.id);
+  });
+
+  //listens when there's an error detected and logs the error on the console
+  client.on("error", function (err) {
+    console.log("Error detected", client.id);
+    console.log(err);
+  });
 });
 
-//Default connected status
-let connected = false;
-
-// Connect the client to the server
-async function buildConnection(){
-  connected = connected ? true : await client.connect();
-}
-
-async function closeConnection(){
-    await client.close();
-}
-
-async function findUsers(filter={}) {
-  await buildConnection()
-  const db = client.db('users')
-  const usersCollection = db.collection('users');
-  const user = usersCollection.find(filter);
-  const usertoArray = await user.toArray();
-  console.log(usertoArray)
-  return usertoArray
-}
-
-async function insertUser(newUser) {
-  await buildConnection()
-  const db = client.db('users')
-  const usersCollection = db.collection('users');
-  const user = await usersCollection.insertOne(newUser);
-  console.log(user)
-}
-
-async function insertUniqueUser(newUser) {
-  await buildConnection()
-  const db = client.db('users')
-  const usersCollection = db.collection('users');
-  const existedUser = await findUsers({account: newUser["account"]})
-
-  if (existedUser.length > 0) {
-    console.log("User existed")
-    await client.close();
-    return false
-  } else {
-    const user = await usersCollection.insertOne(newUser);
-    console.log(user["ops"])
-    return true
-  }
-}
-
-async function updateUser(user,attributes) {
-  await buildConnection()
-  const db = client.db('users')
-  const usersCollection = db.collection('users');
-  const updatedUser = await usersCollection.updateOne(user,{$set:attributes});
-  console.log(updatedUser)
-}
-
-async function findMessages(user,channel){
-  await buildConnection()
-  const db = client.db('chat')
-  const usersCollection = db.collection('users');
-  const users = usersCollection.find(user);
-  const usertoArray = await users.toArray();
-  console.log(usertoArray)
-  if (usertoArray.length > 0) {
-    const userAccount = usertoArray[0]["account"];
-    const messagesCollection = db.collection('messages');
-    const allMessages = messagesCollection.find({account:userAccount});
-    const allMessagestoArray = await allMessages.toArray();
-    console.log(allMessagestoArray)
-    return allMessagestoArray
-  } else {
-    return false
-  }
-}
-
-async function insertMessages(user,newMessages){
-  await buildConnection()
-  const db = client.db('chat')
-  const messagesCollection = db.collection('messages');
-  const userAccount = user["account"];
-  const messageContent = newMessages["text"];
-  const currentTime = getCurrentTime();
-  const messages = {
-    "account": userAccount,
-    "text": messageContent,
-    "cdate":currentTime,
-    "updatedDatetime": currentTime
-  }
-  console.log(messages)
-  await messagesCollection.insertOne(messages);
-  return true
-}
-
-async function updateMessages(messageId,newText){
-  await buildConnection()
-  const db = client.db('chat')
-  const messagesCollection = db.collection('messages');
-  const updateContent = {
-    $set: {
-      "text": newText,
-    }
-  };
-  await messagesCollection.updateOne({_id: messageId},updateContent,{ upsert: false })
-  .then((res)=>{
-    return true})
-  .catch((err)=>{
-    console.log(err)
-    return false});
-
-  // const targetMessage = messagesCollection.find({_id: messageId});
-  // const targetMessagestoArray = await targetMessage.toArray();
-  // console.log(targetMessagestoArray)
-  // const userAccount = user["account"];
-  // const messageContent = message["text"];
-
-}
-
-// findMessages({account:'yc9'})
-// .then(async (result)=>{
-//   await closeConnection()
-// })
-
-// updateMessages('60bd9c7b2b7712e25e5f3746','12345tyhnmlo87trdsxcvbnjkuy')
-// .then(async (result)=>{
-//   await closeConnection()
-// })
-
-module.exports = {
-  buildConnection,
-  closeConnection,
-  findUsers,
-  insertUser,
-  insertUniqueUser,
-  updateUser,
-  findMessages,
-  insertMessages,
-  updateMessages
-}
-// insertUniqueUser(
-//   {"account" : "yc12",
-//   "password" : "at575dang350",
-//   "firstName" : "Xin",
-//   "lastName" : "Chang",
-//   "email" : "j123456159@gmail.com",
-//   "id" : "1"}
-// ).then(async (result)=>{
-//   await closeConnection()
-// })
-
-// insertMessages(
-//   {"account":"yc9"},
-//   {"text":"So Sad!!"}
-// ).then(async (result)=>{
-//   await closeConnection()
-// })
+var port = process.env.PORT || 3000;
+httpServer.listen(port, function (err) {
+  if (err) console.log(err);
+  console.log("Listening on port", port);
+});
--- mongo/utils.js
+++ mongo/utils.js
@@ -1,8 +1,7 @@
-const moment = require('moment-timezone');
-
+const moment = require("moment-timezone");
 
 function getCurrentTime() {
   return moment.tz(Date.now(), "Asia/Taipei").format("YYYY-MM-DD HH:mm:ss");
 }
 
-module.exports = { getCurrentTime }
+module.exports = { getCurrentTime };
